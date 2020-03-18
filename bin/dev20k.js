2fc5be6af996f0234f582fa38c03b6b4f03ae998
sebdybowski <dybowski@int.pl>
2020-03-18 19:50:49 +0100

    Automatic changes

--- bin/dev20k.js
+++ bin/dev20k.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-
-const esm = require('esm')(module/*, options*/);
-module.exports = esm('./../lib/');
+// eslint-disable-next-line no-global-assign
+require = require('esm')(module/*, options*/);
+module.exports = require('./../lib/index');
